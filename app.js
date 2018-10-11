b1054ce362950fdc368ec2c2760b8314dd594d62
Javier Rodriguez <ajr1986@gmail.com>
2018-10-11 12:15:00 -0300

    Add CORS

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ var imageRoutes = require('./routes/image');
 // Init variables
 var app = express();
 
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Body Parser
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
